310f8885beb2e796702cd1362e4709c31da566d7
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-26 11:29:22 +0200

    Corregge ordine parole

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,9 +1,9 @@
 "use strict"
 let firstWord = prompt("inserisci la prima parola");
 let secondWord = prompt("inserisci la seconda parola");
-if(firstWord.length>secondWord.length){
+if(firstWord.length<secondWord.length){
     alert(`${firstWord} ${secondWord}`);
-}else if (secondWord.length>firstWord.length){
+}else if (secondWord.length<firstWord.length){
     alert(`${secondWord} ${firstWord}`);
 }else{
     alert("le parole hanno la stessa lunghezza");
