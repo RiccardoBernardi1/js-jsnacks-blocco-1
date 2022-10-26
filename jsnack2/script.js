9e197dbeed73227642fa7da078db6432dc4ddb8f
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-26 11:02:37 +0200

    Corregge esercizio

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,9 +2,9 @@
 let firstWord = prompt("inserisci la prima parola");
 let secondWord = prompt("inserisci la seconda parola");
 if(firstWord.length>secondWord.length){
-    alert("la prima parola è più lunga");
+    alert(`${firstWord} ${secondWord}`);
 }else if (secondWord.length>firstWord.length){
-    alert("la seconda parola è più lunga");
+    alert(`${secondWord} ${firstWord}`);
 }else{
     alert("le parole hanno la stessa lunghezza");
 }
